05fb737c8958400ea16d556aff13e5cd38cb7eec
Subhasis-hacker <subhasisjena1909@gmail.com>
2026-09-17 23:45:48 +0530

    change in base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const apiClient = axios.create({
-  baseURL: 'https://code-reviewer-backend-yxx4.onrender.com/',
+const api = axios.create({
+    baseURL: "https://code-reviewer-backend-yxx4.onrender.com/api/v1"
 });
 
 // Auto-inject JWT token to every protected request
