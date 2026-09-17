7213a5c121baf2aec95489fdc6f976c5e186da56
Subhasis-hacker <subhasisjena1909@gmail.com>
2026-09-17 19:22:01 +0530

    add frontend url

--- backend/main.py
+++ backend/main.py
@@ -50,10 +50,14 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173", 
+        # Local development
+        "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+
+        # Production frontend
+        "https://code-reviewer-beige.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
